47ef2df74489d6db8b5dd5c3b31c2cc52c48fbf4
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-03-18 15:24:27 -0400

    Bring back classic outputs

--- openstack/outputs.tf
+++ openstack/outputs.tf
@@ -2,3 +2,40 @@
 output "public_instances" {
   value = { for x, values in local.instances : x => openstack_compute_floatingip_associate_v2.fip[x].floating_ip if contains(values.tags, "public") }
 }
+
+output "cluster_name" {
+  value = lower(var.cluster_name)
+}
+
+output "domain" {
+  value = lower(var.domain)
+}
+
+output "sudoer_username" {
+  value = var.sudoer_username
+}
+
+output "freeipa_passwd" {
+  value = random_string.freeipa_passwd.result
+}
+
+output "guest_usernames" {
+  value = "user[${format(format("%%0%dd", length(tostring(var.nb_users))), 1)}-${var.nb_users}]"
+}
+
+output "guest_passwd" {
+  value = var.guest_passwd != "" ? var.guest_passwd : try(random_pet.guest_passwd[0].id, "")
+}
+
+# output "rsa_public_key" {
+#   value = tls_private_key.login_rsa.public_key_openssh
+# }
+
+output "ssh_private_key" {
+  value     = try(tls_private_key.ssh[0].private_key_pem, null)
+  sensitive = true
+}
+
+output "proxy_ids" {
+  value = local.proxy_ids
+}
